e25951f648f6638c7f3db1e9479502ac4f253fc2
pareot <55153435+pareot@users.noreply.github.com>
2021-10-11 13:17:47 -0700

    w/e

---
+++ triangle.js
@@ -0,0 +1,17 @@
+// let counter = 0;
+//
+// for (var i = 0; i < 6; i++) {
+//   for (var t = 0; t < 6; t++) {
+//     while(t <= counter) {
+//       console.log("#");
+//       counter++;
+//     }
+//   }
+// }
+
+
+let mark='';
+for (var i = 0; i <= 6; i++) {
+  mark += '#'
+  console.log(mark);
+}
