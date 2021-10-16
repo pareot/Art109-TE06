401c397ee7949c307815e775fd13cc3e6754846a
pareot <55153435+pareot@users.noreply.github.com>
2021-10-15 22:48:21 -0700

    completed in class group exercise

---
+++ inclass2.js
@@ -0,0 +1,10 @@
+// function for exercise 2
+const makingTriangle = function () {
+  let mark='';
+  for (var i = 0; i <= 6; i++) {
+    mark += '#';
+    console.log(mark);
+  }
+};
+makingTriangle();
+console.log("test");
---
+++ inclass3.js
@@ -0,0 +1,9 @@
+// function for 3rd reading
+var x = 10;
+var y = 100;
+
+const returnMin = function(x, y) {
+  console.log(Math.min(x, y));
+};
+returnMin(x, y);
+console.log("test");
